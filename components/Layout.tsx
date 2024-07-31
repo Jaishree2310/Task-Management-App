714bef7da6e7c749836434e60d1f5054e47e1fc7
Jaishree2310 <jaishrees23102001@gmail.com>
2024-07-31 23:49:45 +0530

    new changes

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,42 @@
-import React from 'react';
-import Sidebar from './Sidebar';
-import TaskBoard from './TaskBoard';
+// import React from 'react';
+// import Sidebar from './Sidebar';
+// import TaskBoard from './TaskBoard';
 
-const Layout: React.FC = () => {
+// const Layout: React.FC = () => {
+//   return (
+//     <div className="layout">
+//       <Sidebar />
+//       <main className="main-content">
+    
+//         <TaskBoard />
+//       </main>
+//     </div>
+//   );
+// };
+
+// export default Layout;
+
+
+
+
+
+
+
+
+
+import React, { ReactNode } from 'react';
+import Sidebar from './Sidebar';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="layout">
       <Sidebar />
       <main className="main-content">
-    
-        <TaskBoard />
+        {children}
       </main>
     </div>
   );
